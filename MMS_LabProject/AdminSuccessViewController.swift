a84fbb6fd4b6566f2a89f7f93e7767bccbbff63e
christineputri <christineputri21@gmail.com>
2023-12-05 15:06:30 +0700

    awe

---
+++ MMS_LabProject/AdminSuccessViewController.swift
@@ -0,0 +1,31 @@
+//
+//  AdminSuccessViewController.swift
+//  MMS_LabProject
+//
+//  Created by prk on 05/12/23.
+//
+
+import UIKit
+
+class AdminSuccessViewController: UIViewController {
+
+    @IBAction func logoutButton(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
