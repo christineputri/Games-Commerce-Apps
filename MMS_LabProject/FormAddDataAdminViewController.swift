50d978539d8ee3fa6f8dd1b9abee6f656f15497f
william4312 <gunawanandreas02@gmail.com>
2023-12-04 22:51:21 -0800

    constrain add product

---
+++ MMS_LabProject/FormAddDataAdminViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FormAddDataAdminViewController.swift
+//  MMS_LabProject
+//
+//  Created by prk on 12/4/23.
+//
+
+import UIKit
+
+class FormAddDataAdminViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
