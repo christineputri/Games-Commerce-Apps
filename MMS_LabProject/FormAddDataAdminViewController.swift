dfd2b46112026aee6a7dab0167f25c820949b028
pavellsan8 <pavelryan146@gmail.com>
2023-12-06 09:14:58 +0700

    ni uda rapi

--- MMS_LabProject/FormAddDataAdminViewController.swift
+++ MMS_LabProject/FormAddDataAdminViewController.swift
@@ -1,14 +1,31 @@
-//
-//  FormAddDataAdminViewController.swift
-//  MMS_LabProject
-//
-//  Created by prk on 12/4/23.
-//
-
 import UIKit
 
-class FormAddDataAdminViewController: UIViewController {
+class FormAddDataAdminViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var priceTextField: UITextField!
+    
+    var imageTemp = "mobile_legend"
+    
+    @IBAction func addImage(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let imageData = info[.editedImage] as? UIImage else {return}
+        imageTemp = imageData.imageURL
+        dismiss(animated: true)
+    }
+    
+    @IBAction func addDataToDatabase(_ sender: Any) {
+        arr.append(dataItem(priceProduct: Int(priceTextField.text), titleProduct: titleTextField.text, categoryProduct: CategoryGame.FPS, description: descriptionTextField.text, imageProduct: image))
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
