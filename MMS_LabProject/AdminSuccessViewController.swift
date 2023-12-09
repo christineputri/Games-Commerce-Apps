53f69d798cdd9ab5c656c86566d97f02ff503d8d
pavellsan8 <pavelryan146@gmail.com>
2023-12-09 15:54:30 +0700

    add data oke, gambar below

--- MMS_LabProject/AdminSuccessViewController.swift
+++ MMS_LabProject/AdminSuccessViewController.swift
@@ -8,12 +8,10 @@ class AdminSuccessViewController: UIViewController {
     }
     
     @IBAction func backHomeBtn(_ sender: Any) {
-        navigateToAwal()
-    }
-    
-    private func navigateToAwal() {
-            if let ViewController = storyboard?.instantiateViewController(withIdentifier: "ViewController") {
-                navigationController?.pushViewController(ViewController, animated: true)
-            }
+        if let nextView = storyboard?.instantiateViewController(identifier: "adminView") {
+            let adminView = nextView as! AdminViewController
+
+            navigationController?.setViewControllers([adminView], animated: true)
         }
+    }
 }
--- MMS_LabProject/CustomerViewController.swift
+++ MMS_LabProject/CustomerViewController.swift
@@ -16,7 +16,7 @@ class CustomerViewController: UIViewController, UITableViewDataSource, UITableVi
         cell.itemTitleProduct.text = arr[indexPath.row].titleProduct
         cell.itemCategoryProduct.text = arr[indexPath.row].categoryProduct.rawValue
         cell.itemDetailProduct.text = arr[indexPath.row].description
-        cell.itemPriceProduct.text = "Rp. \(arr[indexPath.row].priceProduct)"
+        cell.itemPriceProduct.text = "$\(arr[indexPath.row].priceProduct)"
         cell.itemImageProduct.image = UIImage(named: arr[indexPath.row].imageProduct)
         
         return cell
@@ -26,10 +26,10 @@ class CustomerViewController: UIViewController, UITableViewDataSource, UITableVi
     var arr: [dataItem] = []
     
     func initData(){
-        arr.append(dataItem(priceProduct: 2000, titleProduct: "Mobile Legend", categoryProduct: CategoryGame.FPS, description: "Family Friendly Game", imageProduct: "mobile_legend"))
-        arr.append(dataItem(priceProduct: 5000, titleProduct: "Mobile Legend", categoryProduct: CategoryGame.FPS, description: "Family Friendly Game", imageProduct: "mobile_legend"))
-        arr.append(dataItem(priceProduct: 7000, titleProduct: "Mobile Legend", categoryProduct: CategoryGame.FPS, description: "Family Friendly Game", imageProduct: "mobile_legend"))
-        arr.append(dataItem(priceProduct: 27000, titleProduct: "Mobile Legend", categoryProduct: CategoryGame.FPS, description: "Family Friendly Game", imageProduct: "mobile_legend"))
+        arr.append(dataItem(priceProduct: 300, titleProduct: "God Of War : Ragnarok", categoryProduct: CategoryGame.adventure, description: "Adventure story games about Gods", imageProduct: "godofwar"))
+        arr.append(dataItem(priceProduct: 250, titleProduct: "Red Dead Redemption 2", categoryProduct: CategoryGame.adventure, description: "Open world game for you who want to be cowboy", imageProduct: "rdr2"))
+        arr.append(dataItem(priceProduct: 500, titleProduct: "The Last Of Us", categoryProduct: CategoryGame.horror, description: "Survival story games in the zombie apocalypse", imageProduct: "tlou"))
+        arr.append(dataItem(priceProduct: 200, titleProduct: "Uncharted 4", categoryProduct: CategoryGame.adventure, description: "Adveture story games and parkour mode", imageProduct: "uncharted4"))
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
